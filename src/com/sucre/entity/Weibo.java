673250cbdcd4ef422d4a889c0cc40ec7ee8aab9d
youmi <906509023@qq.com>
2018-11-06 13:04:08 +0800

    Initial commit

---
+++ src/com/sucre/entity/Weibo.java
@@ -0,0 +1,71 @@
+package com.sucre.entity;
+
+import com.sucre.utils.MyUtil;
+
+abstract public class Weibo {
+	private String id;
+	private String pass;
+	private String cookie;
+	private String uid;
+	private String s;
+	
+	// 抽象方法,要求子类必须覆盖.
+	//abstract public int doWork(int index);
+		
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getCookie() {
+		return cookie;
+	}
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	public String toString() {
+		return "Weibo [id=" + id + ", pass=" + pass + ", cookie=" + cookie + ", uid=" + uid + ", s=" + s + "]";
+	}
+	public Weibo() {
+		super();
+	}
+	public Weibo(String id,String pass){
+		this.id=id;
+		this.pass=pass;
+	}
+	//从文件数据里加载数据！
+	public Weibo(String inputdata){
+		try{
+		String[] temp = inputdata.split("\\|");
+		this.cookie=temp[0];
+		this.uid=temp[1];
+		this.id = temp[3];
+		this.pass = temp[4];
+		this.s=temp[5];
+		}catch (Exception e) {
+			MyUtil.print("导入weibo数据出错", null);
+		}
+	}
+	
+	
+}
---
+++ src/com/sucre/utils/Printer.java
@@ -0,0 +1,5 @@
+package com.sucre.utils;
+
+public interface Printer {
+	public void print(String data);
+}
