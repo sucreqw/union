80e7a5ca3a4cf3e363cb903ac8f97c8b8a48cea2
sucre <906509023@qq.com>
2018-12-01 14:18:12 +0800

    union

--- src/com/sucre/service/PlayStatistics.java
+++ src/com/sucre/service/PlayStatistics.java
@@ -7,7 +7,16 @@ public class PlayStatistics {
 	// 加油卡，点亮
 		private byte[] incrspt(String cookie, String vid,String uid ) {
 			
-			
+			/*Dim D As String
+			Dim Sig, Ks As String
+
+			'Math.random().toString(36).substring(5, 10) + "_" + +(new Date) +"_"+ Math.floor(Math.random() * 1e4) + d)
+			Ks = "H5_" & MakeNonce(5) & "_" & GetTimesB() & MakeNumber(4) & "1"
+
+			Sig = md5("{""uid"":""" & U & """,""mid"":""" & M & """,""keys"":""" & K & """,""type"":""feedvideo""}" & Ks & "yixiong&zhaolong5")
+
+			D = "data={""uid"":""" & U & """,""mid"":""" & M & """,""keys"":""" & K & """,""type"":""feedvideo""}&key=" & Ks & "&sig=" & Sig
+*/
 			StringBuilder data = new StringBuilder(900);
 			String temp ="data="+ vid +"&key=H5_o814g_154364091935018541&sig=72e17bd5371a75acd501d5457182dd6d\r\n";
 			data.append("POST https://weibo.com/aj/video/playstatistics?ajwvr=6 HTTP/1.1\r\n");
