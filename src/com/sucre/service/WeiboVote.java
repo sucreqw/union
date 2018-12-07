5e706fc66779e800af12b658ae5a3c2d048fdfdc
youmi <906509023@qq.com>
2018-12-07 16:22:33 +0800

    union

--- src/com/sucre/service/WeiboVote.java
+++ src/com/sucre/service/WeiboVote.java
@@ -296,6 +296,8 @@ public class WeiboVote extends Weibo {
 	private byte[] starhit(String vid) {
 		StringBuilder data = new StringBuilder(900);
 		//String temp = "";
+		//data.append("GET https://mbd.baidu.com/webpage?type=starhit&action=vote&format=json&starid="+ vid +"&votetype=1 HTTP/1.1\r\n");
+		                                 //GET /webpage?type=starhit&action=vote&format=json&starid=3772&votetype=1 HTTP/1.1
 		data.append("GET https://mbd.baidu.com/webpage?type=starhit&action=vote&format=json&starid="+ vid +"&votetype=1 HTTP/1.1\r\n");
 		data.append("Host: mbd.baidu.com\r\n");
 		data.append("Connection: keep-alive\r\n");
@@ -304,7 +306,8 @@ public class WeiboVote extends Weibo {
 		data.append("Accept: */*\r\n");
 		data.append("Referer: https://mbd.baidu.com/webpage?type=starhit&action=starhome&starid="+ vid +"&from=alading\r\n");
 		data.append("Accept-Language: zh-CN,en-US;q=0.9\r\n");
-		data.append("Cookie: BDUSS="+ MyUtil.makeNonce(12)+"Vk9XLXVtNHFWRlhKTXF4MkxmU3NwMX54dld"+ MyUtil.makeNonce(6)+"2llRWNYUkpjQVFBQUFBJCQAAAAAAAAAAAEAAA"+ MyUtil.makeNonce(26)+"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+ MyUtil.makeNonce(14)+"S;\r\n");
+		data.append("Cookie: BAIDUCUID=_avet_P1SulP"+ MyUtil.makeNonce(12)+"-"+ MyUtil.makeNonce(12)+"_iE-NN(10)Pf_uL28gha2tDA;  BAIDUID="+ MyUtil.makeNonce(12)+"9759B8EAEA14CFC65FCC:FG=1; BDUSS="+ MyUtil.makeNonce(12)+"Vk9XLXVtNHFWRlhKTXF4MkxmU3NwMX54dld"+ MyUtil.makeNonce(6)+"2llRWNYUkpjQVFBQUFBJCQAAAAAAAAAAAEAAA"+ MyUtil.makeNonce(26)+"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+ MyUtil.makeNonce(14)+"S;\r\n");
+		//data.append("Cookie: BAIDUCUID=_avet_P1SulPavu3guSNuluP-aYVu2iAg82o8_iE-NN(10)Pf_uL28gha2tDA;  BAIDUID=5E0954C4180E9759B8EAEA14CFC65FCC:FG=1;  BDUSS=mhRQ05ZYkNVVk9XLXVtNHFWRlhKTXF4MkxmU3NwMX54dldpRjdxR1llRWNYUkpjQVFBQUFBJCQAAAAAAAAAAAEAAADWq945AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABzQ6lsc0OpbS;\r\n");
 		data.append("X-Forwarded-For: "+ MyUtil.getIp()+"\r\n");
 		data.append("\r\n");
 		data.append("\r\n");
@@ -313,10 +316,10 @@ public class WeiboVote extends Weibo {
 	
 	// 剧赞
 		private byte[] gouvote(String uid, String cookie, String s, String vid) {
-			StringBuilder data = new StringBuilder(900);
-
+			StringBuilder data = new StringBuilder(900);  
+			uid=MyUtil.makeNumber(10);
 			data.append(
-					"GET /2/page/button?request_url=http%3A%2F%2Fi.dianshi.weibo.com%2Fji_gouvote%3Fmid%3D"+ vid +"%26uid%3D"+ uid +"%26active_id%3D3015%26na_id%3D1001&networktype=wifi&accuracy_level=0&cardid=Square_DoubleButton&uicode=10000011&moduleID=700&featurecode=10000085&wb_version=3654&c=android&i=f842b7a&s="+ s +"&ft=11&ua=HUAWEI-Che2-TL00__weibo__8.6.3__android__android4.4.2&wm=9006_2001&aid=01Anlv2XwdpcqURzkYptXmiLjU9xJv2UnDaKr12aNYUkBHuVU.&fid=231219_3015_newartificial_1001&v_f=2&v_p=62&from=1086395010&gsid="+ cookie +"&lang=zh_CN&lfid=231522type%3D1%26q%3D%23%E7%94%B5%E8%A7%86%E5%89%A7%E5%A4%A7%E8%B5%8F%23%26t%3D3&skin=default&oldwm=9006_2001&sflag=1&luicode=10000003 HTTP/1.1\r\n");
+					"GET /2/page/button?request_url=http%3A%2F%2Fi.dianshi.weibo.com%2Fji_gouvote%3Fmid%3D"+ vid +"%26uid%3D"+ uid +"%26active_id%3D3015%26na_id%3D1001&networktype=wifi&accuracy_level=0&cardid=Square_DoubleButton&uicode=10000011&moduleID=700&featurecode=10000085&wb_version=3654&c=android&i=f842b7a&s="+ s +"&ft=11&ua=HUAWEI-Che2-TL00__weibo__8.6.3__android__android4.4.2&wm=9006_2001&aid=01Anlv2XwdpcqURzk"+ MyUtil.makeNonce(32)+".&fid=231219_3015_newartificial_1001&v_f=2&v_p=62&from=1086395010&gsid="+ cookie +"&lang=zh_CN&lfid=100303type%3D1%26q%3D%E5%89%A7%E8%B5%9E%26t%3D0&skin=default&oldwm=9006_2001&sflag=1&luicode=10000003 HTTP/1.1\r\n");
 			data.append("Host: api.weibo.cn\r\n");
 			data.append("Connection: keep-alive\r\n");
 			data.append(
